45faceb0266fbe36941eb247a7f3af1d70437d55
Shridhigupta13 <shridhigupta22@gmail.com>
2026-09-16 11:42:37 +0530

    Time: 0 ms (100%), Space: 43.6 MB (27.29%) - LeetHub

---
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int uniquePathsWithObstacles(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+
+        int [][] dp = new int[m+1][n+1];
+        if(grid[0][0] == 1)
+        return 0;
+        dp[0][0] = 1; 
+
+        //condition for the first column
+        for(int  i=1; i<m; i++){
+            if(grid[i][0] == 0)
+            dp[i][0] = dp[i-1][0];
+        }
+
+        //condition for the first row
+        for(int j=1; j<n; j++){
+            if(grid[0][j] == 0 )
+            dp[0][j] = dp[0][j-1];
+        }
+
+        //baacche huwe cells k liaaa
+        for(int i=1; i<m; i++){
+            for(int j=1; j<n; j++){
+
+                if(grid[i][j] == 0){
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1];
+                    
+                }
+
+            }
+        }
+        return dp[m-1] [n-1];
+    }
+}
